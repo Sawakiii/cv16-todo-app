e255b2c68a021af6058be289a9c241cd40677bbe
Sawaki <you@example.com>
2020-02-07 15:30:22 +0900

    コンポーネント作成

---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form action="submit">
+        <input type="text"/>
+        <input type="text"/>
+        <button type="submit">追加</button>
+    </form>
+  )
+}
+
+export default Form
---
+++ src/Item.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Item = () => {
+  return (
+    <p>宿題をやる</p>
+  )
+}
+
+export default Item
